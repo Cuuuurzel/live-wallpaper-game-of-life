4914cfef5a74e31aea5fc9786c51d445f3f7b01e
Cuuuurzel <fcurzel94@gmail.com>
2013-06-15 17:12:18 +0200

    Fixed Project

---
+++ GolLiveWallpaper/gen/com/cuuuurzel/gollivewallpaper/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.cuuuurzel.gollivewallpaper;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- LiveWallpaperGameOfLife/src/com/cuuuurzel/livewallpapergameoflife/GameOfLife.java
+++
@@ -1,5 +0,0 @@
-package com.cuuuurzel.livewallpapergameoflife;
-
-public class GameOfLife {
-
-}
